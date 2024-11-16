91b5b42eeb2371406b795c91cfa4211f7330d9da
unknown <tt.shinas06@gmail.com>
2024-11-16 23:53:28 +0530

    wishlist updated

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,27 +1,71 @@
 //rafce
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToWishlist } from '../redux/slices/wishlistSlice'
 
 const View = () => {
+  const dispatch = useDispatch()
+  const userWishlist = useSelector(state=>state.wishlistReducer)
+  const [product,setProduct] = useState({})
+  const {id} = useParams()
+  // console.log(id);
+  // console.log(product);  
+  
+  useEffect(()=>{
+    if(sessionStorage.getItem("allProducts")){
+      const allProducts = JSON.parse(sessionStorage.getItem("allProducts"))
+      // console.log(allProducts.find(item=>item.id==id));
+      setProduct(allProducts.find(item=>item.id==id))
+    }
+  },[])
+
+  const handleWishlist= ()=>{
+    const existingProduct = userWishlist?.find(item=>item?.id==id)
+    if(existingProduct){
+      alert("Product already in your wishlist!!!")
+    }else{
+      alert("Product added to your wishlist")
+      dispatch(addToWishlist(product))
+    }
+  }
+  
   return (
     <>
     <Header/>
-    <div className='flex flex-col mx-5' >
+    <div className='flex flex-col mx-5 pt-5' >
       <div className="grid grid-cols-2 items-center h-screen">
-        <img className='ms-40' width={'350px'} height={'250px'} src="https://tse1.mm.bing.net/th?id=OIP.ddFM4RqUwzewdhXm00fcewHaEJ&pid=Api&P=0&h=180" alt="" />
         <div>
-          <h3 className='font-bold'>PID : id </h3>
-          <h1 className='text-5xl font-bold'>Title </h1>
-          <h4 className="font-bold text-red-600 text-2xl">$ 250</h4>
-          <h4>Brand : brand</h4>
-          <h4>Category : category</h4>
-          <p>
-            <span className='font-bold'>Description </span> : Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae at alias totam voluptatum perspiciatis quas excepturi omnis ipsam ex suscipit. Laborum placeat veniam maxime vero fugiat cumque modi veritatis natus? 
-          </p>
-          <div className="flex justify-between mt-5">
-            <button className="bg-blue-600 rounded text-white p-2">ADD TO WISHLIST</button>
-            <button className="bg-green-600 rounded text-white p-2">ADD TO CART</button>
+          <img className='ms-40' width={'350px'} height={'250px'} src={product?.thumbnail} alt="" />
+          <div className="flex justify-between m-5 ">
+              <button onClick={handleWishlist} className="bg-blue-600 rounded text-white p-2">ADD TO WISHLIST</button>
+              <button className="bg-green-600 rounded text-white p-2">ADD TO CART</button>
           </div>
+        </div>
+        <div>
+          <h3 className='font-bold'>PID : {product?.id} </h3>
+          <h1 className='text-5xl font-bold'>{product?.title} </h1>
+          <h4 className="font-bold text-red-600 text-2xl">$ {product?.price}</h4>
+          <h4>Brand : {product?.brand}</h4>
+          <h4>Category : {product?.category}</h4>
+          <p>
+            <span className='font-bold'>Description </span> : {product?.description} 
+          </p>
+          <h3 className="font-bold mt-4">Client Reviews</h3>
+          {
+            product?.reviews?.length>0?
+              product?.reviews?.map(item=>(
+                <div key={item?.date} className="shadow border rounded p-2 mb-2">
+                  <h5>
+                    <span className="font-bold"> {item?.reviewerName} </span> : <span> {item?.comment } </span>
+                  </h5>
+                  <p>Rating : {item?.rating} <i className="fa-solid fa-star text-yellow-400"></i> </p>
+                </div>
+              ))
+            :
+            <div className="font-bold text-red-600">No Reviews yet!!!</div>
+          }
         </div>
       </div>
     </div>
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,27 +1,43 @@
 //rafce
 import React from 'react'
 import Header from '../components/Header'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeItem } from '../redux/slices/wishlistSlice'
 
 const Wishlist = () => {
+  const dispatch = useDispatch()
+  const userWishlist = useSelector(state=>state.wishlistReducer)
   return (
     <>
       <Header/>
       <div style={{paddingTop:'100px'}} className='px-5'>
+      {
+        userWishlist?.length>0 ?
         <>
-          <h1 className="text-4xl font-bold text-red-600">My Wishlist</h1>
-          <div className="grid grid-cols-4 gap-4 mt-5">
-            <div className="rounded border p-2 shadow">
-              <img width={'100%'} height={'200px'} src="https://tse1.mm.bing.net/th?id=OIP.ddFM4RqUwzewdhXm00fcewHaEJ&pid=Api&P=0&h=180" alt="" />
+        <h1 className="text-4xl font-bold text-red-600">My Wishlist</h1>
+        <div className="grid grid-cols-4 gap-4 mt-5">
+         {
+          userWishlist?.map(product=>(
+            <div key={product?.id} className="rounded border p-2 shadow">
+              <img width={'100%'} height={'200px'} src={product?.thumbnail} alt="" />
               <div className="text-center">
-                <h3 className="text-xl font-bold">title</h3>
+                <h3 className="text-xl font-bold">{product?.title}</h3>
                 <div className="flex justify-evenly mt-3">
-                  <button className="text-xl"> <i className="fa-solid fa-heart-circle-xmark text-red-500"></i> </button>
+                  <button onClick={()=>dispatch(removeItem(product?.id))} className="text-xl"> <i className="fa-solid fa-heart-circle-xmark text-red-500"></i> </button>
                   <button className="text-xl"> <i className="fa-solid fa-cart-plus text-green-700"></i> </button>
                 </div>
               </div>
             </div>
-          </div>
+          ))
+         }
+        </div>
         </>
+      :
+      <div className="flex flex-col justify-center items-center">
+        <img className='w-100 h-1/2' src="https://www.adanione.com/~/media/Foundation/Adani/emptyImages/empty_cart.gif" alt="" />
+        <h1 className="text-4xl text-red-600 mt-3">Your Wishlist is Empty!!!</h1>
+      </div>
+       }
       </div>
      
     </>
